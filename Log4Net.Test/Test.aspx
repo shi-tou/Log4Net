1f2244268f7245e3fdabba5d05497a968f1b7ba5
yksoft <gx_ylb@126.com>
2016-04-15 14:16:54 +0800

    Initial commit

---
+++ Log4Net.Test/Test.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="Log4Net.Test.Test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button runat="server" ID="btnError" Text="Error" OnClick="btnError_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Log4Net.Test/Test.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Log4Net.Test
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnError_Click(object sender, EventArgs e)
+        {
+            LogHelper.Debug("Debug-Message");
+            LogHelper.Debug("Exception", new Exception("这是一个Exception"));
+            LogHelper.Debug("测试Format:{0}","12345678");
+
+            LogHelper.Error("Debug-Message");
+            LogHelper.Error("Exception", new Exception("这是一个Exception"));
+            LogHelper.Error("测试Format:{0}", "12345678");
+
+            LogHelper.Info("Debug-Message");
+            LogHelper.Info("Exception", new Exception("这是一个Exception"));
+            LogHelper.Info("测试Format:{0}", "12345678");
+
+            LogHelper.Warn("Debug-Message");
+            LogHelper.Warn("Exception", new Exception("这是一个Exception"));
+            LogHelper.Warn("测试Format:{0}", "12345678");
+        }
+    }
+}
